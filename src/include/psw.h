6ff434d276c335f13e6d2cc4856fc09ebd1205a5
George <60898611+nomadrussian@users.noreply.github.com>
2024-04-04 11:37:54 +0400

    Update psw.h
    added #pragma once to avoid potential collisions

--- src/include/psw.h
+++ src/include/psw.h
@@ -1,4 +1,5 @@
 /* psw.h */
+#pragma once
 
 const char *VERSION = "v0.4";
 const char *LAST_UPD = "August 14th, 2023";
@@ -8,4 +9,3 @@ void proceedHelp(int argc, char **argv);
 void proceedGen(int argc, char **argv);
 void proceedGenlist(int argc, char **argv);
 int getGenFlags(int argc, char **argv, int shift);
-
